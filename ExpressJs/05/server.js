d3e8ab926006555195f33c879c54836ea8abf076
codermada <tolotra025@gmail.com>
2026-06-30 09:06:43 +0300

    ExpressJs/05 app.sendFile path.resolve

--- ExpressJs/05/server.js
+++ ExpressJs/05/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.use(express.static('./public'))
+
 app.get('/', function(req, res){
-    res.send("Hello world");
+    res.sendFile(path.resolve(__dirname, "./index.html"));
 });
 
 app.listen(3000, "0.0.0.0", () => {
