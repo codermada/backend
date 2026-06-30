5cc2bd0a8c2847662221646473d72a94fcc06d8b
codermada <tolotra025@gmail.com>
2026-06-30 09:49:26 +0300

    ExpressJs/05 morgan

--- ExpressJs/05/server.js
+++ ExpressJs/05/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const morgan = require('morgan');
 
 app.use(express.static('./public'))
+app.use(morgan('dev'));
+
 
 app.get('/', function(req, res){
     res.sendFile(path.resolve(__dirname, "./index.html"));
